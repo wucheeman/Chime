fe6c66cad63c533dceb9c84817f7a6bc2e53a4ae
Mark Hainline <wucheeman@gmail.com>
2018-06-06 11:54:57 -0400

    updated to run seeds file + instructions on how to stop background server process

--- setupDB.sh
+++ setupDB.sh
@@ -1,23 +1,40 @@
 #!/bin/bash”
+
+# kills server.js when script exits
+trap "kill 0" EXIT
+
 cd db
 mysql -u mark < schema.sql
 
-# cd back out to root
+cd ../
 # run server.js in background to create tables
+# edit to read 'server.js' when Matt creates it
+node mah_server.js &
 
-# cd back in to db/
-# mysql -u mark burgerzDB < seeds.sql
+# give MySQL some time to build the table
+sleep 3
+
+cd db
+mysql -u mark chimedb < seeds.sql
 
 # do a select * on each table that's been populated?
 
 cd ..
-# stop server.js
+# script waits until we kill it
+wait
+
 
 
 # INSTRUCTIONS
+# To run:
+# ========
 # cd to the project's root directory. 
 # Enter ($ is the prompt; ignore the text in parentheses):
 #
 #  $ bash ./setupDB.sh (Windows)
 #
 #  $ ./setupDB.sh (Mac/Linux)
+#
+# To stop
+# =======
+# Use CTRL-C
