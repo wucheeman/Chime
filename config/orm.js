0932f38ce24a52f1f76fc8ad55cc6fb4b7669f63
Matthew Buchanan <matt@matthews-air.home>
2018-06-11 15:09:18 -0400

    database without sequelize initial commit

---
+++ config/orm.js
@@ -0,0 +1,50 @@
+var connection = require('./connection.js');
+
+var orm = {
+  selectAll: function(table, cb) {
+    qv = `SELECT * FROM ${table}`;
+    query(qv, cb);
+  },
+  findAll: function(table, col, val, cb) {
+    qv = `SELECT * FROM ${table}`;
+    qv += ` WHERE ${val} = ${col};`;
+    query(qv, cb);
+  },
+  insertRow: function(table, colArr, valArr, cb) {
+    qv = `INSERT INTO ${table} (`;
+    qv += arrIntoQuery(colArr);
+    qv += `) VALUES (`;
+    qv += arrIntoQuery(valArr);
+    query(qv, cb);
+  },
+  updateVal: function(table, id, col, val, cb) {
+    var qv = `UPDATE ${table} WHERE id=${id}`;
+    qv += ` SET ${col} = ${val}`;
+    query(qv, cb);
+  },
+  deleteRow: function(table, id, cb) {
+    var qv = `DELETE FROM ${table} WHERE id=${id}`;
+    query(qv, cb);
+  }
+}
+
+function query(queryVal, cb) {
+  connection.query(queryVal, (err, result) => {
+    if (err) throw err;
+    cb(result);
+  });
+}
+
+function arrIntoQuery(arr) {
+  var queryVal = ''
+  for (var i = 0; i < queryVal.length-1; i++) {
+    queryVal += `${arr[i]}`;
+    if (i < queryVal.length - 1) {
+      queryVal += `,`;
+    }
+  }
+  return queryVal;
+}
+
+
+module.exports = orm;
