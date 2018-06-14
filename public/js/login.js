b0f292230524376ccd1e3b48c4d54acac82b3a58
Matthew Buchanan <matt@matthews-air.home>
2018-06-14 16:03:24 -0400

    incorporated jumbotron and dropdown menu of recent pull request

--- public/js/login.js
+++ public/js/login.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
+  var path = 'user';
+  $('.entity').click(function(event) {
+    event.preventDefault();
+    path = $(this).attr("data-value");
+  });
+
   $('#login').click(function(event) {
     event.preventDefault();
     $('#login-success').css('visibility', 'hidden');
     var input = {
-      entity: $('input[name="entity"]:checked').val(),
+      entity: path,
       username: $('#username').val().trim(),
     };
     $.ajax('/login', {
