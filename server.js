40a51b6b880e507b4133be1d07871150e7644e94
Neeni Nagaraj <neeninagaraj@Neenis-MacBook-Air.local>
2018-06-14 19:37:38 -0400

    update server.js, main.handlebars, and login

--- server.js
+++ server.js
@@ -17,13 +17,12 @@ var initializeDB = require('./config/initializeDB.js');
 initializeDB();
 
 // Use logger for testing routes
-/*
+
 var logger = function(req, res, next) {
   console.log(req.method + ' ' + req.path + ' ' + JSON.stringify(req.body));
   next();
 }
 app.use(logger);
-*/
 
 require("./controllers/routes.js")(app);
 
